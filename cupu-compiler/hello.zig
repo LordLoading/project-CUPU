e8cef06408df82dd5e0dff767c1a8b0611f7b656
LordLoading <lorenzdressel@gmail.com>
2024-10-23 08:36:11 +0200

    stack planning

--- cupu-compiler/hello.zig
+++ cupu-compiler/hello.zig
@@ -1,45 +1,13 @@
 const std = @import("std");
-const cli = @import("zig-cli");
-
-// Define a configuration structure with default values.
-var config = struct {
-    host: []const u8 = "localhost",
-    port: u16 = undefined,
-}{};
+const warn = std.debug.warn;
+const allocator = std.debug.global_allocator;
+const process = std.process;
 
 pub fn main() !void {
-    var r = try cli.AppRunner.init(std.heap.page_allocator);
+    var arg_it = process.args();
 
-    // Create an App with a command named "short" that takes host and port options.
-    const app = cli.App{
-        .command = cli.Command{
-            .name = "short",
-            .options = &.{
-                // Define an Option for the "host" command-line argument.
-                .{
-                    .long_name = "host",
-                    .help = "host to listen on",
-                    .value_ref = r.mkRef(&config.host),
-                },
+    // skip my own exe name
+    _ = arg_it.skip();
 
-                // Define an Option for the "port" command-line argument.
-                .{
-                    .long_name = "port",
-                    .help = "port to bind to",
-                    .required = true,
-                    .value_ref = r.mkRef(&config.port),
-                },
-            },
-            .target = cli.CommandTarget{
-                .action = cli.CommandAction{ .exec = run_server },
-            },
-        },
-    };
-    return r.run(&app);
-}
-
-// Action function to execute when the "short" command is invoked.
-fn run_server() !void {
-    // Log a debug message indicating the server is listening on the specified host and port.
-    std.log.debug("server is listening on {s}:{d}", .{ config.host, config.port });
+    std.debug.print(arg_it, .{});
 }
